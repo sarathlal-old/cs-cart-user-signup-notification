65439ead142d588c1a36c659b5ec7a6abaddac30
Sarathlal <sarathlaln88@gmail.com>
2016-06-09 12:21:54 +0530

    Option to add notification email

---
+++ README.md
@@ -0,0 +1,9 @@
+# Admin Notification on User Sign Up
+
+This addon will send an email notification to User department / predefined email address on new user sign up.
+
+On the addon settings page, there is option to enable / disable notification. If there is a valid email ID is entered on addon settings, the notifications are send to that email ID. Else Notification goes in to User Department Email.
+
+Tested on CS Cart 4.3.
+
+Check my website for [free addons, code tweaks & hacks](http://sarathlal.com).
--- app/addons/csc_signup_notification/func.php
+++ app/addons/csc_signup_notification/func.php
@@ -7,12 +7,12 @@ use Tygh\Http;
 use Tygh\Settings;
 use Tygh\Mailer;
 
-function fn_csc_signup_notification_send_notification($status, $user_id){
+function fn_csc_signup_notification_send_notification($status, $user_id, $to_email){
     $user_data = fn_get_user_info($user_id);
             if (AREA != 'A' and $status == 'A') {
                 Mailer::sendMail(array(
 
-                    'to' => 'company_users_department',
+                    'to' => $to_email,
                     'from' => 'company_users_department',
                     'reply_to' => $user_data['email'],
                     'data' => array(
@@ -28,7 +28,15 @@ function fn_csc_signup_notification_update_profile($action, $user_data, $current
     if ($action == 'add' && $user_data['user_type'] == 'C') {
         if (!fn_allowed_for('ULTIMATE:FREE')) {
 			if(Registry::get('addons.csc_signup_notification.enable_notification') == 'Y'){
-				fn_csc_signup_notification_send_notification('A', $user_data['user_id']);
+				$to_email = Registry::get('addons.csc_signup_notification.notification_email');
+				if((!empty($to_email)) and (!filter_var($to_email, FILTER_VALIDATE_EMAIL) === false)){
+					$notifier = $to_email;
+				} else { 
+					$notifier = 'company_users_department';
+				}
+				
+				
+				fn_csc_signup_notification_send_notification('A', $user_data['user_id'], $notifier);
 			}
         }
     }
